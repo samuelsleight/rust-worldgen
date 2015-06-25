1e0ec63274a3f8efc6fe620367f6b5c61b4afa53
Sam Sleight <samuel.sleight@gmail.com>
2015-06-25 18:12:28 +0100

    Added licence

--- src/noise/coherent.rs
+++ src/noise/coherent.rs
@@ -1,3 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////
+//  File: rust-worldgen/noise/coherent.rs
+//////////////////////////////////////////////////////////////////////////////
+//  Copyright 2015 Samuel Sleight
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//////////////////////////////////////////////////////////////////////////////
+
 //! A provider of coherent noise.
 //! This generates a noise value for each corner of the unit square the given
 //! point is in, and then calculates the noise value based on these.
